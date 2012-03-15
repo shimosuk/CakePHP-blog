cc3f917341055f391c37af18c853736deed6ef02
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-03-15 14:37:43 +0900

    インデントの修正

--- Controller/PostsController.php
+++ Controller/PostsController.php
@@ -23,26 +23,26 @@ class PostsController extends AppController {
         }
     }
 
-   public function edit($id = null) {
-    $this->Post->id = $id;
-    if ($this->request->is('get')) {
-        $this->request->data = $this->Post->read();
-    } else {
-        if ($this->Post->save($this->request->data)) {
-            $this->Session->setFlash('Your post has been updated.');
-            $this->redirect(array('action' => 'index'));
+    public function edit($id = null) {
+        $this->Post->id = $id;
+        if ($this->request->is('get')) {
+            $this->request->data = $this->Post->read();
         } else {
-            $this->Session->setFlash('Unable to update your post.');
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash('Your post has been updated.');
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash('Unable to update your post.');
+            }
         }
     }
-}
-public function delete($id) {
-    if ($this->request->is('get')) {
-        throw new MethodNotAllowedException();
+    public function delete($id) {
+        if ($this->request->is('get')) {
+            throw new MethodNotAllowedException();
+        }
+        if ($this->Post->delete($id)) {
+            $this->Session->setFlash('The post with id: ' . $id . ' has been deleted.');
+            $this->redirect(array('action' => 'index'));
+        }
     }
-    if ($this->Post->delete($id)) {
-        $this->Session->setFlash('The post with id: ' . $id . ' has been deleted.');
-        $this->redirect(array('action' => 'index'));
-    }
-}
 }

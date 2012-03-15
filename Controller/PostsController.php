5473b6f1fee6c86db48d8801e4885fa133f838dc
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-03-15 11:41:16 +0900

    データベース参照して表示する

--- Controller/PostsController.php
+++ Controller/PostsController.php
@@ -4,5 +4,6 @@ class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 
     public function index() {
+        $this->set('posts', $this->Post->find('all'));
     }
 }
